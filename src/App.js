f85574ef39b3dd88121a8f1301cec0c9bc3dcd72
Le Thanh Dat <lethanhdat1567@gmail.com>
2024-10-30 16:56:36 +0700

    Fix basename

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         );
     };
     return (
-        <Router basename="/engcom">
+        <Router>
             <div className="App">
                 <ScrollToTop />
                 {!user.id && handleRoute(publicPage)}
