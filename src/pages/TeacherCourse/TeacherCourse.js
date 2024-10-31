80d06e3be75c2f3712d7b706d1268be0887bf12a
Le Thanh Dat <lethanhdat1567@gmail.com>
2024-10-31 10:13:22 +0700

    Fix active lesson

--- src/pages/TeacherCourse/TeacherCourse.js
+++ src/pages/TeacherCourse/TeacherCourse.js
@@ -10,13 +10,13 @@ function TeacherCourse() {
         return item.lesson_id == activeLesson.id;
     });
     switch (activeLesson.type) {
-        case 0: {
+        case '0': {
             return <TeacherLessonVideo data={data} />;
         }
-        case 1: {
+        case '1': {
             return <TeacherLessonText data={data} />;
         }
-        case 2: {
+        case '2': {
             return <TeacherLessonPractice data={data} />;
         }
         default: {
